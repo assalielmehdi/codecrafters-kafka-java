35b558e474fead61f852e049bdfd2b471e8d64c6
El Mehdi ASSALI <assalielmehdi@gmail.com>
2025-02-10 00:53:25 +0100

    feat: Accept multiple sequential and concurrent requests

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,25 +3,35 @@ import handlers.RequestRouter;
 import message.request.KafkaRequestMessage;
 import types.Int32;
 
+import java.io.IOException;
 import java.net.ServerSocket;
+import java.net.Socket;
 
 public class Main {
-  public static void main(String[] args) throws Exception {
-    var serverSocket = new ServerSocket(9092);
-    var clientSocket = serverSocket.accept();
+  public static void main(String[] args) {
+    try (var serverSocket = new ServerSocket(9092)) {
+      while (true) {
+        var clientSocket = serverSocket.accept();
+        Thread.ofVirtual().start(() -> handleConnection(clientSocket));
+      }
+    } catch (IOException ignored) {}
+  }
 
-    var requestLengthBytes = clientSocket.getInputStream().readNBytes(4);
-    var requestBytes = ToBytes.join(
-      requestLengthBytes,
-      clientSocket.getInputStream().readNBytes(Int32.fromBytes(requestLengthBytes).value())
-    );
-    var request = KafkaRequestMessage.fromBytes(requestBytes);
+  private static void handleConnection(Socket clientSocket) {
+    try {
+      var requestLengthBytes = new byte[0];
+      while ((requestLengthBytes = clientSocket.getInputStream().readNBytes(4)).length > 0) {
+        var requestBytes = ToBytes.join(
+          requestLengthBytes,
+          clientSocket.getInputStream().readNBytes(Int32.fromBytes(requestLengthBytes).value())
+        );
+        var request = KafkaRequestMessage.fromBytes(requestBytes);
 
-    var response = new RequestRouter().route(request);
-    var responseBytes = response.toBytes();
-    clientSocket.getOutputStream().write(responseBytes);
-
-    clientSocket.close();
-    serverSocket.close();
+        var response = new RequestRouter().route(request);
+        var responseBytes = response.toBytes();
+        clientSocket.getOutputStream().write(responseBytes);
+        clientSocket.getOutputStream().flush();
+      }
+    } catch (IOException ignored) {}
   }
 }
